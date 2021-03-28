5369c6f73aa3697ec2cfd7225d4cf9101b2f477f
weekyear <jwnsgus@gmail.com>
2021-03-28 20:37:09 +0900

    0328 -S : boj_11066, boj_11066_knuth

--- 2021/03/28/boj_11066_파일 합치기_knuth.py
+++ 2021/03/28/boj_11066_파일 합치기_knuth.py
@@ -14,11 +14,22 @@ for _ in range(int(input())):
     for n in range(1, N + 1):
         sums[n] = sums[n - 1] + nums[n - 1]
 
+    knuth = [[0] * N for _ in range(N)]
+    for k in range(N):
+        knuth[k][k] = k
+
     for gap in range(1, N + 1):
         for i in range(N - gap):
             j = i + gap
             dp[i][j] = 0xffffffffff
-            for k in range(i, j):
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + sums[j + 1] - sums[i])
+            for k in range(knuth[i][j-1], knuth[i + 1][j] + 1):
+                if k >= N - 1:
+                    break
+
+                cur_val = dp[i][k] + dp[k + 1][j] + sums[j + 1] - sums[i]
+                if dp[i][j] > cur_val:
+                    dp[i][j] = cur_val
+                    knuth[i][j] = k
+
 
     print(dp[0][N - 1])
